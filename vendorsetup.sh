9a64f75c409e3ca0dd3c5fcbcfc907458705e77a
T-rex2017 <Tonykchristopher@gmail.com>
2018-10-20 08:18:52 +0530

    auto apply patches
    apply patches at the init by Dhiraj MS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,10 @@
 for var in eng user userdebug; do
   add_lunch_combo lineage_P1m-$var
 done
+
+#apply patches at init by Dhiraj MS
+
+#dir
+cd device/lenovo/P1m/patches
+#apply
+bash apply-patches.sh
