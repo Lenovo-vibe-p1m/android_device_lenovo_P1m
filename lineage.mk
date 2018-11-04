479019469e0f255d65c60ad4f079265864bbbe9b
T-rex2017 <Tonykchristopher@gmail.com>
2018-11-04 15:44:22 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -3,8 +3,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := P1m
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common cm stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/P1m/device_P1m.mk)
